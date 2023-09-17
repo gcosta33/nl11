41470d114f4dad5326288e5d79c3138424f70808
Guilherme Martins <guilherme3394@gmail.com>
2023-09-17 20:19:15 -0300

    desing header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { Button } from "./components/ui/button";
+import { Github } from 'lucide-react'
+import { Separator } from "./components/ui/separator";
 
 export function App() {
   return (
-    <h1>
-      Hello word
-    </h1>)
+    <div className="min-h-screen flex flex-col">
+      <div id="header" className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ia</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido with ❤️</span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div></div>
+      </main>
+    </div>
+  )
 }
 
